355c754cc39f624ee480147261cd14656494cf38
a-nshul <upadhyay041298@gmail.com>
2024-12-04 12:32:16 +0530

    changed some code for deployement

--- config/db.js
+++ config/db.js
@@ -2,17 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("Mongo URI:", process.env.MONGODB_URI); // Check if it's being loaded
-    if (!process.env.MONGODB_URI) {
-      throw new Error("MONGODB_URI is not defined in .env file");
-    }
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
 };
 
